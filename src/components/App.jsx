ea0bfb28ab30ec61d921fdf856531fe1fb1d613d
Serhii Ozeran <ozeransm@gmail.com>
2023-06-24 23:28:37 +0300

    update task v10

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
-import { Profile } from "./Profile/Profile.jsx";
+import { Profile } from "/Profile/Profile.jsx";
 import user from "components/user.json";
-import { Statistics } from "./Statistics/Statistics.jsx";
+import { Statistics } from "/Statistics/Statistics.jsx";
 import data from "components/data.json";
 import friendsData from "components/friends.json";
-import { FriendList } from "./Friends/FriendList.jsx";
+import { FriendList } from "/Friends/FriendList.jsx";
 import transactionHistory from "components/transactions.json";
-import { TransactionHistory } from "./TransactionHistory/TransactionHistory.jsx";
+import { TransactionHistory } from "/TransactionHistory/TransactionHistory.jsx";
 const { username, tag, location, avatar, stats }=user;
 
 export const App = () => {
